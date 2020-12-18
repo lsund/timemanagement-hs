f1a00a75a90d6c3035c272a26358d4c4da31d642
Ludvig Sundstrom <ludvig.sundstroem@innoq.com>
2020-12-18 16:16:24 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,50 @@
+# timemanagement-hs
+
+Aggregates times in a logfile
+
+## Install / Usage
+
+Requires haskell build tool like `stack` or `cabal`, alternatively
+`runhaskell`.
+
+
+```
+# Install e.g.
+stack install
+
+# Run
+~/.local/bin/timemanagement ~/times.txt
+
+# Or like this
+runhaskell app/Main.hs  ~/times.txt
+```
+
+`times.txt` is a text file with lines of `hh:mm <project>`.
+
+E.g. for this file:
+
+```
+> cat times.txt
+
+08:46 Pause
+10:02 Pause
+10:02 stop
+10:33 Project1: foo
+13:17 Project1: foo
+14:18 Pause
+14:19 Project2: bar
+14:21 Project2: bar
+14:22 Pause
+15:42 Project1: foo
+```
+
+
+```
+> runhaskell app/Main.hs  ~/times.txt
+("Pause",157)
+("Project1: foo",225)
+("Project2: bar",3)
+("stop",31)
+```
+
+
---
+++ app/Main.hs
@@ -0,0 +1,54 @@
+module Main where
+
+import qualified Data.Map.Strict as M
+import Data.Map.Strict (Map)
+import Data.List.Split (splitOn)
+import System.Environment (getArgs)
+
+file :: FilePath
+file = "/home/lsund/Data/log/time/log_20201218_auto.txt"
+
+type Time = (Int, Int)
+type Activity = String
+
+data Entry = Entry { _time :: Time, _activity :: Activity }
+    deriving (Show)
+
+aggregateMinutes :: [Entry] -> Map Activity Int
+aggregateMinutes (e : es) =
+    fst $ foldl
+        (\(acc, (Entry (h', m') a')) entry@(Entry (h, m) a) ->
+            case M.lookup a' acc of
+              Just a'' -> (M.adjust (+ minuteDiff (h', m') (h, m)) a' acc, entry)
+              Nothing -> (M.insert a' (minuteDiff (h', m') (h, m)) acc, entry))
+        (M.empty, e) es
+
+
+-- First argument is smaller than the second
+minuteDiff :: Time -> Time -> Int
+minuteDiff (h1, m1) (h2, m2) = ((h2 - h1) * 60) + m2 - m1
+
+
+makeEntry :: [String] -> Maybe Entry
+makeEntry [] = Nothing
+makeEntry [x] = Nothing
+makeEntry (x : xs) = Just $ Entry (makeTime x) (unwords xs)
+    where
+          makeTime x =
+              let [hours, minutes] = splitOn ":" x -- FIXME bad pattern match
+               in (read hours :: Int, read minutes :: Int)
+
+main :: IO ()
+main = do
+    args <- getArgs
+    if (not . null) args
+       then
+        do
+            let file = head args
+            content <- readFile file
+            case mapM (makeEntry . words) $ lines content of
+                Just es -> (mapM_ print . M.toList . aggregateMinutes) es
+                Nothing -> putStrLn $ "Problem reading " <> file
+       else
+            putStrLn "Usage: ./timemanagement-hs FILE"
+
